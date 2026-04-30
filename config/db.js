d548cb2eed7b953f2c00660f5d4d4d78e44db4ae
-França- <gustavo.santos568.franca@gmail.com>
2026-04-29 21:46:50 -0300

    arrumando a poha do multer

--- config/db.js
+++ config/db.js
@@ -1,47 +1,22 @@
+// config/db.js
 const sqlite3 = require('sqlite3');
 const { open } = require('sqlite');
-const path = require('path');
 
 async function initDB() {
     const db = await open({
-        filename: path.join(__dirname, '../doceria.sqlite'),
+        filename: './doceria.sqlite',
         driver: sqlite3.Database
     });
 
+    // AQUI ESTAVA A BOMBA! Agora adicionamos o 'imagem TEXT'
     await db.exec(`
         CREATE TABLE IF NOT EXISTS produtos (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            nome TEXT NOT NULL,
+            nome TEXT,
             descricao TEXT,
-            preco REAL NOT NULL,
-            imagem_url TEXT
-        );
-
-        CREATE TABLE IF NOT EXISTS clientes (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            nome TEXT NOT NULL,
-            telefone TEXT NOT NULL,
-            endereco TEXT NOT NULL
-        );
-
-        CREATE TABLE IF NOT EXISTS pedidos (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            cliente_id INTEGER,
-            data_pedido DATETIME DEFAULT CURRENT_TIMESTAMP,
-            status TEXT DEFAULT 'Pendente',
-            valor_total REAL NOT NULL,
-            FOREIGN KEY (cliente_id) REFERENCES clientes(id)
-        );
-
-        CREATE TABLE IF NOT EXISTS itens_pedido (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            pedido_id INTEGER,
-            produto_id INTEGER,
-            quantidade INTEGER NOT NULL,
-            preco_unitario REAL NOT NULL,
-            FOREIGN KEY (pedido_id) REFERENCES pedidos(id),
-            FOREIGN KEY (produto_id) REFERENCES produtos(id)
-        );
+            preco REAL,
+            imagem TEXT
+        )
     `);
 
     return db;
